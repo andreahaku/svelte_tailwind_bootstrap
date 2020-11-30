633771c706d5779408544b3b9be0b71ec15cd5e6
Andrea Salvatore <andreahaku@gmail.com>
2020-11-30 11:22:38 +0000

    Initial setup

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+</script>
+
+<main>
+	<div class="py-8 px-4 bg-indigo-100 border border-indigo-500 shadow-lg">
+		This is a card styled with TailwindCSS
+	</div>
+</main>
+
+<style>
+</style>
